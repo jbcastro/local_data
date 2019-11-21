84acb0b4b1174235e9d1d9eef7c980c06fc9f3b9
jbcastro <jbeckcastro810@southseattle.edu>
2019-11-21 12:53:52 -0800

    Updates

---
+++ precache-manifest.9c9ea9c104f9e6a02c5e08a2450849c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "336eda3f56141da78bb16f81243a24d6",
+    "url": "/local_data/index.html"
+  },
+  {
+    "revision": "bbac3ed5181c30732357",
+    "url": "/local_data/static/css/main.4b1ac18f.chunk.css"
+  },
+  {
+    "revision": "9434ac0f4f386640e766",
+    "url": "/local_data/static/js/2.03416529.chunk.js"
+  },
+  {
+    "revision": "bbac3ed5181c30732357",
+    "url": "/local_data/static/js/main.568e6026.chunk.js"
+  },
+  {
+    "revision": "03116561b0580ed11c0f",
+    "url": "/local_data/static/js/runtime~main.5bb3b91c.js"
+  }
+]);
