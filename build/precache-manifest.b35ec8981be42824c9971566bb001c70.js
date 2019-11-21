09cd2e2b0830eecf1c93403f4e66a200a2c4eb0f
jbcastro <jbeckcastro810@southseattle.edu>
2019-11-21 13:04:56 -0800

    new build

--- build/precache-manifest.b35ec8981be42824c9971566bb001c70.js
+++ build/precache-manifest.b35ec8981be42824c9971566bb001c70.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "336eda3f56141da78bb16f81243a24d6",
+    "revision": "8a05d316f68ea74b9067cb84a178156d",
     "url": "/local_data/index.html"
   },
   {
-    "revision": "bbac3ed5181c30732357",
-    "url": "/local_data/static/css/main.4b1ac18f.chunk.css"
+    "revision": "7aa5925e5d0d8ef14377",
+    "url": "/local_data/static/css/main.902d613a.chunk.css"
   },
   {
     "revision": "9434ac0f4f386640e766",
     "url": "/local_data/static/js/2.03416529.chunk.js"
   },
   {
-    "revision": "bbac3ed5181c30732357",
+    "revision": "7aa5925e5d0d8ef14377",
     "url": "/local_data/static/js/main.568e6026.chunk.js"
   },
   {
